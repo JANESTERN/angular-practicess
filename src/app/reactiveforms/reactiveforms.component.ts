7faaa2303c294ac3762901cee22c68602cf9f79f
unknown <wrightkuku10@gmail.com>
2023-02-21 18:03:12 +0300

    more on reactive forms

--- src/app/reactiveforms/reactiveforms.component.ts
+++ src/app/reactiveforms/reactiveforms.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-reactiveforms',
@@ -9,23 +10,65 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class ReactiveformsComponent implements OnInit{
 
   form!: FormGroup
+
+  usernames=['janestern', 'wright', 'Anonymous', 'Test']
+
+  emails=['janestern@gmail.com', 'wright.@gmail.com', 'Anonymous.@gmail.com', 'Test.@gmail.com']
+
   ngOnInit(): void{
     this.form=new FormGroup({
       personalDetails: new FormGroup({
-        username:new FormControl(null, [Validators.required]),
+        username:new FormControl(null, [Validators.required, this.unallowedPersons.bind(this) ]),
         email:new FormControl(null, [Validators.required, Validators.email]),
         phone:new FormControl(null, [Validators.required]),
       }),
-      postiton:new FormControl('Default',[Validators.required]),
+      position:new FormControl('Default',[Validators.required]),
       message:new FormControl(null, [Validators.required]),
-
+      hobbies:new FormArray([])
     })
+
 
   }
 submitData(){
   console.log(this.form);
-  
+}
+
+addHobbies(){
+  let control=new FormControl(null,[Validators.required]);
+  (this.form.get('hobbies')as FormArray).push(control)
+}
+gethobbies(){
+  return (this.form.get("hobbies") as FormArray).controls
+}
+
+removecontrol(i:number){
+  (this.form.get("hobbies") as FormArray).removeAt(i)
 
 }
 
+unallowedPersons(control:FormControl): {[s:string]:boolean} | null{
+  if(this.usernames.indexOf(control.value)!==-1){
+    return {forbidden:true}
+  }
+ return null     
 }
+
+checkingExistingEmails(control:FormControl):Promise<{[s:string]:boolean} |null> | Observable <{[s:string]:boolean} |null>{
+  const promise=new Promise<{[s:string]:boolean} |null>(resolve,reject)=>{
+    setTimeout(()=>{
+      if(this.emails.indexOf(control.value)!==-1){
+        resolve({emailfound: true})
+      }else{
+        resolve(null)
+      }
+
+    },2000)
+  }
+  return promise
+
+}
+
+
+}
+
+
